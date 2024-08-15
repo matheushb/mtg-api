bd274a06805b3779189dd1211415176c4049cb6e
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-08-14 22:24:25 -0300

    feat: add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { AppController } from './app.controller';
+import { BcryptModule } from './common/bcrypt/bcrypt.module';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from './common/config/env-config';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    BcryptModule,
+    ConfigModule.forRoot({ load: [envConfig], isGlobal: true }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
