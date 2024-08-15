d9103981459c66fe6390a43440504d3b131b1ae0
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-08-14 22:25:53 -0300

    feat: add user crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
+@ApiTags('users')
 @Controller()
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll() {
+    const users = await this.findAll();
+
+    return {
+      data: users,
+      meta: {
+        total: users.length,
+      },
+    };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.findOne(id);
+    return {
+      data: user,
+    };
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.update(id, updateUserDto);
+    return {
+      data: updatedUser,
+    };
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    await this.delete(id);
+  }
+}
