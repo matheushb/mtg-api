a0fe7584408db823d76bd9ba35a69db982b5de58
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-08-14 22:25:39 -0300

    feat: add bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { BcryptService } from 'src/common/bcrypt/bcrypt.service';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
@@ -7,11 +8,12 @@ export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly bcryptService: BcryptService,
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findByEmail(username);
-    if (user && user.password === pass) {
+    if (user && this.bcryptService.compare(pass, user.password)) {
       const { password, ...result } = user;
       return result;
     }
