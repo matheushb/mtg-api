9532d029a800414a021def53a29a571253dad928
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-08-14 22:24:37 -0300

    temp: add future queries to main

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import appConfig from './common/config/app-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //https://api.scryfall.com/cards/search?q=type:legendary+type:creature&order=usd query
+  //https://api.scryfall.com/cards/search?q=color=W+-type:legendary&order=usd query 2
   appConfig(app);
   await app.listen(3000);
 }
