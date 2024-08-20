26e7a77890c49094e05b8f52e2b90f6d8bca4150
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-08-20 20:40:02 -0300

    feat: finished seed method

--- src/gateways/scyfall.gateway.ts
+++ src/gateways/scyfall.gateway.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { appendFileSync } from 'fs';
+import { writeFileSync } from 'fs';
 
 @Injectable()
 export class ScyfallGateway {
@@ -17,36 +17,83 @@ export class ScyfallGateway {
       'search?q=type:legendary+type:creature&order=random',
     );
 
-    const leader = await leaderResponse.data.data[0];
+    const leader = await leaderResponse.data.data[
+      Math.ceil(Math.random() * 20)
+    ];
 
     const colors = leader.colors.join('');
+    const cards = [];
+
+    cards.push({
+      id: leader.id,
+      name: leader.name,
+      released_date: leader.released_at,
+      mana_cost: leader.mana_cost,
+      type: leader.type_line,
+      text: leader.oracle_text,
+      power: leader.power,
+      toughness: leader.toughness,
+      colors: leader.colors,
+      cmc: leader.cmc,
+      keywords: leader.keywords,
+      rarity: leader.rarity,
+      price_in_usd: leader.prices.usd ?? 0,
+      foil_price_in_usd: leader.prices.usd_foil ?? 0,
+    });
 
     const cardsResponse = await this.axios.get(
-      `search?q=color=${colors}+-type:legendary&order=random&limit=99`,
+      `search?q=color=${colors}+-type:legendary&order=random`,
     );
 
-    const cards = await cardsResponse.data.data.slice(0, 99).map((card) => {
-      console.log(card);
+    let x = 0;
+    while (cards.length < 100 && x++ <= cardsResponse.data.data.length) {
+      const card = cardsResponse.data.data[x];
 
-      const parsedCart = {
-        id: card.id,
-        name: card.name,
-        released_date: card.released_at,
-        mana_cost: card.mana_cost,
-        type: card.type_line,
-        text: card.oracle_text,
-        power: card.type_line in ['Instant', 'Sorcery'] ? card.power : 0,
-        toughness:
-          card.type_line in ['Instant', 'Sorcery'] ? card.toughness : 0,
-        colors: card.colors,
-        cmc: card.cmc,
-        keywords: card.keywords,
-        rarity: card.rarity,
-        price_in_usd: card.prices.usd ?? 0,
-        foil_price_in_usd: card.prices.usd_foil ?? 0,
-      };
-      console.log(parsedCart);
-      return parsedCart;
-    });
+      if (card.name.includes('//')) continue;
+
+      if (
+        card.type_line === 'Instant' ||
+        card.type_line === 'Sorcery' ||
+        card.type_line.includes('Enchantment') ||
+        card.type_line.includes('Artifact')
+      ) {
+        cards.push({
+          id: card.id,
+          name: card.name,
+          released_date: card.released_at,
+          mana_cost: card.mana_cost,
+          type: card.type_line,
+          text: card.oracle_text,
+          colors: card.colors,
+          cmc: card.cmc,
+          keywords: card.keywords,
+          rarity: card.rarity,
+          price_in_usd: card.prices.usd ?? 0,
+          foil_price_in_usd: card.prices.usd_foil ?? 0,
+        });
+      } else {
+        cards.push({
+          id: card.id,
+          name: card.name,
+          released_date: card.released_at,
+          mana_cost: card.mana_cost,
+          type: card.type_line,
+          text: card.oracle_text,
+          power: card.power,
+          toughness: card.toughness,
+          colors: card.colors,
+          cmc: card.cmc,
+          keywords: card.keywords,
+          rarity: card.rarity,
+          price_in_usd: card.prices.usd ?? 0,
+          foil_price_in_usd: card.prices.usd_foil ?? 0,
+        });
+      }
+    }
+
+    console.log(cards);
+    writeFileSync('deck.json', JSON.stringify(cards, null, 2));
+    console.log(cards.length);
+    console.log(leader);
   }
 }
