3936dfaf38677d8586b5b7b3238ba5e43ff174c0
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-09-07 17:14:30 -0300

    fix: add findOne on jwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy } from 'passport-jwt';
+import { UserService } from 'src/modules/user/user.service';
+import { UserFromRequest } from '../auth.service';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +16,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: {
+    email: string;
+    sub: string;
+    role: Role;
+    iat: number;
+    exp: number;
+  }) {
+    await this.userService.findOne(payload.sub);
+
     return { id: payload.sub, email: payload.email, role: payload.role };
   }
 }
