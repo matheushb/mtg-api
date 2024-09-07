703b8bac00cdea15e017d8991fb08f93b7e0c07e
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-09-07 16:21:44 -0300

    refactor: remove logs

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -12,13 +12,11 @@ export class RolesGuard implements CanActivate {
       context.getHandler(),
       context.getClass(),
     ]);
-    console.log(requiredRoles);
     if (!requiredRoles) {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
 
-    console.log(user);
     return requiredRoles.some((role) => user.role?.includes(role));
   }
 }
