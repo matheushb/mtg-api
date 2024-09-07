d004e3876e2daf1079fc389ad0eaea2a9a2917cf
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-09-07 15:28:23 -0300

    feat: add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { AppController } from './app.controller';
@@ -7,13 +7,19 @@ import { BcryptModule } from './common/bcrypt/bcrypt.module';
 import { ConfigModule } from '@nestjs/config';
 import envConfig from './common/config/env-config';
 import { ScyfallModule } from './gateways/scyfall.module';
+import { CardsModule } from './modules/cards/cards.module';
+import { DecksModule } from './modules/deck/cards.module';
+import { CardDeckModule } from './modules/card-deck/card-deck.module';
 
 @Module({
   imports: [
-    UserModule,
     AuthModule,
+    UserModule,
     PrismaModule,
     BcryptModule,
+    CardsModule,
+    DecksModule,
+    CardDeckModule,
     ScyfallModule,
     ConfigModule.forRoot({ load: [envConfig], isGlobal: true }),
   ],
