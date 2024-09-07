b3a4a29f139a152f823f6781439d7afb30ba8e19
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-09-07 15:30:18 -0300

    feat: add auth

--- src/gateways/scyfall.controller.ts
+++ src/gateways/scyfall.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ScyfallGateway } from './scyfall.gateway';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @ApiTags('Scyfall')
 @Controller('scyfall')
 export class ScyfallController {
